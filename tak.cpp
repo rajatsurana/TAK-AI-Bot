16b2d2d6d5e6cdac11b20988fa3869d688c7157c
rajatsurana <rajat.surana123@gmail.com>
2016-09-26 00:35:12 +0530

    struct to class matrix

--- tak.cpp
+++ tak.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 #define size 5
 
-struct Matrix{
+class Matrix{
 	public:
 		vector<int> arrArrVect[size][size];
+		validMove(int type,int i, int j,int dirn);
+		insert(int i,int j,int type);
 		// 11 white flat stone
 		// 12 black flat stone
 		// 21 white standing stone
@@ -13,8 +15,17 @@ struct Matrix{
 		// 31 white cap stone
 		// 32 black cap stone
 };
+
+void Matrix::validMove(int type,int i, int j,int dirn){//type=(in/out)
+	int posX=i;
+	int posY=j;
+	int direction=dirn;
+}
+void Matrix::insert(int i,int j,int type){
+	arrArrVect[i][j].push_back(type);
+}
 int main(){
     Matrix matrix;
-    matrix.arrArrVect[0][0].push_back(1);
+    matrix.insert(0,0,11);
     cout<< matrix.arrArrVect[0][0].at(0)<<endl;
 }
